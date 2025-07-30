4d58e7e4f3b142814a75c4963479d5fb062b6594
JaydenHollissSAE <159383304+JaydenHollissSAE@users.noreply.github.com>
2025-07-30 23:46:33 +1000

    Update LobbyButtons.cs

--- Assets/Scripts/LobbyButtons.cs
+++ Assets/Scripts/LobbyButtons.cs
@@ -3,6 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class LobbyButtons : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.visible = true;
+    }
     public void JoinGameMode(string modeName)
     {
         SceneManager.LoadScene(modeName);
